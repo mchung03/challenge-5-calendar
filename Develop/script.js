3564f1aba8c0fb57db4d8ac264e6f62b57cd7ee4
mchung03 <minhee.chung14@gmail.com>
2023-07-11 23:40:52 -0700

    added photos and updated README

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
   // Displays current date in header
   setInterval(function() {
     $("#currentDay").text(dayjs().format('dddd, MMM DD, YYYY'))
   }, 1000)
 
-  
+
   // Adds past, present, future colors to calendar according to current hour
   if(dayjs().hour() > 9) {
     $('#hour-9').addClass('past')
@@ -73,22 +74,26 @@ $(function () {
     $('#hour-17').addClass('future')
   }
 
-  //
-  // var textarea = document.querySelector(".description")
-  // $(".saveBtn").click(function() {
-  //   localStorage.setItem("event", JSON.stringify(textarea));
-  // })
-  
+
+  // After clicking the save button, the input will be saved to local storage
   $(".saveBtn").click(function() {
-    var stored = [];
+    var id = document.getElementById('hour-9');
+    localStorage.setItem('id', id);
 
-    var storedEvent = JSON.parse(localStorage.getItem("stored"));
-    if(storedEvent !== null) {
-      stored = storedEvent;
-    }
-
-    localStorage.setItem("event", JSON.stringify(storedEvent));
+    var description = document.getElementsByClassName('description').value;
+    localStorage.setItem('description', description);
   })
+
+  // $(".saveBtn").click(function() {
+  //   var stored = [];
+
+    // var storedEvent = JSON.parse(localStorage.getItem("stored"));
+    // if(storedEvent !== null) {
+    //   stored = storedEvent;
+    // }
+
+  //   localStorage.setItem("event", JSON.stringify(storedEvent));
+  // })
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # challenge-5-calendar
+
+## Description
+
+This calendar app lets users know the current hour within a 9-5 work schedule, shown by colors. Users can type in an event or to do that they want to plan by the hour and it will save to the local storage so that it will be there for them to refer to. This program was created using HTML, CSS, and JavaScript. 
+
+## Installation
+
+N/A
+
+## Usage
+
+![Calendar Screenshot](./Assets/example.PNG)
+This screenshot is an example of what the webpage would look like if it was 2pm. The hours before 2pm are greyed out, 2pm is red, and the hours after 2pm are green. 
+
+![HTML Screenshot](./Assets/html.PNG)
+Extra boxes were made with the HTML code, up to hour 17, using the same code as the boxes given.
+
+![Day Screenshot](./Assets/current.PNG)
+This code snippet shows the current date when the application is opened.
+
+![Class Screenshot](./Assets/classes.PNG)
+This code snippet makes sure that the color styles are applied to the different hours based on certain conditions, making it possible for the past hours to be grey, present hour to be red, and future hours to be green.
+
+## Credits
+
+dayjs: https://day.js.org/docs/en/get-set/hour
+w3schools: https://www.w3schools.com/jquery/jquery_css_classes.asp
+https://www.w3schools.com/jquery/jquery_events.asp
