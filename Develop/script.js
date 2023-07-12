d71adbb1e05875f3ac5e929ac839fed35b0ea4f9
mchung03 <minhee.chung14@gmail.com>
2023-07-11 18:10:26 -0700

    added code to display current date at header

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,10 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // TODO: Add code to display the current date in the header of the page.
+  setInterval(function() {
+    $("#currentDay").text(dayjs().format('dddd, MMM DD, YYYY'))
+  }, 1000)
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,5 +23,4 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
 });
